3708559f36720d6b971aa6c57e1c9405b69c744c
keighty <k80.leonard@gmail.com>
2014-07-27 12:28:55 -0700

    Add ui for adding new todos.

--- client/views/projects/projects.js
+++ client/views/projects/projects.js
@@ -1,5 +1,20 @@
+Session.set('addTodo', false);
+
 Template.projects.events({
   'click .alert-info': function(e, t) {
     Router.go('projectPage', {_id: this._id});
+  }
+});
+
+Template.projectPage.helpers({
+  addTodo: function() {
+    return Session.equals('addTodo', true);
+  }
+});
+
+Template.projectPage.events({
+  'click .add-todo': function(e, t) {
+    Session.set('addTodo', true);
+    Meteor.flush;
   }
 });
