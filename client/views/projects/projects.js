3d2591e3c12f1912ad88396a4c36729aefff5739
keighty <k80.leonard@gmail.com>
2014-07-27 11:37:08 -0700

    Add navigation from list page to single project page.

--- client/views/projects/projects.js
+++ client/views/projects/projects.js
@@ -3,3 +3,9 @@ Template.projects.helpers({
     return Projects.find({});
   }
 });
+
+Template.projects.events({
+  'click .alert-info': function(e, t) {
+    Router.go('projectPage', {_id: this._id});
+  }
+});
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,7 @@
 Meteor.publish('projects', function() {
   return Projects.find({userId: this.userId});
 });
+
+Meteor.publish('project', function(id) {
+  return Projects.find(id);
+});
