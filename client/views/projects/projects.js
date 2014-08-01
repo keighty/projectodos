fc011d0444a82e7da42f24f9b9f39f46c857a677
keighty <k80.leonard@gmail.com>
2014-08-01 08:20:45 -0700

    Add ability to save new projects.

--- client/views/projects/projects.js
+++ client/views/projects/projects.js
@@ -1,8 +1,35 @@
 Session.set('addTodo', false);
+Session.set('addProject', false);
+
+Template.projects.helpers({
+  addProject: function() {
+    return Session.equals('addProject', true);
+  }
+});
 
 Template.projects.events({
   'click .alert-info': function(e, t) {
     Router.go('projectPage', {_id: this._id});
+  },
+  'click #addProject': function(e, t) {
+    Session.set('addProject', true);
+    Meteor.flush();
+  },
+  'keyup #newProject': function(e, t) {
+    if(e.which == 13) {
+      var newProject = {title: e.target.value};
+      if(newProject.title) {
+        Meteor.call('newProject', newProject, function(error, projectId) {
+          if(error) {
+            throw new Meteor.Error(500, 'Item not saved');
+          } else {
+            e.target.value = '';
+            Session.set('addProject', false);
+            Meteor.flush();
+          }
+        })
+      }
+    }
   }
 });
 
--- collections/projects.js
+++ collections/projects.js
@@ -1,1 +1,29 @@
 Projects = new Meteor.Collection('projects');
+
+Projects.allow({
+  update: ownsDocument,
+  remove: ownsDocument
+});
+
+Meteor.methods({
+  newProject: function(newProject){
+    var user = Meteor.user();
+
+    if(!user)
+      throw new Meteor.Error(401, "You must log in before adding projects");
+
+    var project = _.extend(_.pick(newProject, 'title'), {
+      complete: false,
+      userId: user._id,
+      author: user.username,
+      submitted: new Date().getTime()
+    });
+
+    project._id = Projects.insert(project);
+    if(project._id){
+      return project._id;
+    } else {
+      throw new Meteor.Error(422, "Your project was not saved. Try again.");
+    }
+  }
+});
